e62879904cf0a982e849e0362c50e39c5b1488f3
BrendanClark <iam.clarkbrendan@gmail.com>
2022-11-05 21:55:05 -0400

    finished implementing new ground collision to account better for walls. Added jump buffering and coyote time.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool[] groundLayerTrue;
+
     [SerializeField] private float moveInput;
     [SerializeField] private float moveSpeed;
 
-    [SerializeField] private int jumpsAvailable, maxJumps;
-
+    [SerializeField] private float lastJumpPressed;
+    [SerializeField] private float jumpBuffer;
+    [SerializeField] private float lastGrounded;
+    [SerializeField] private float coyoteTime;
     [SerializeField] private float jumpForce;
 
+    [SerializeField] private bool touchingGround;
     [SerializeField] private bool isGrounded;
+
+    private bool HasBufferedJump => lastJumpPressed + jumpBuffer > Time.time;
+    private bool HasCoyoteTime => lastGrounded + coyoteTime > Time.time;
 
     [Header("Components")]
 
@@ -22,10 +33,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate() {
         Move();
-        isGrounded = GetComponent<GroundCheck>().IsGrounded();
+        if (touchingGround) isGrounded = GetComponent<GroundCheck>().IsGrounded();
+        if (isGrounded) {
+            coyoteTime = Time.time;
+        }
+        if (HasBufferedJump || HasCoyoteTime) Jump();
     }
     private void Move() {
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput * moveSpeed * Time.fixedDeltaTime, rb.velocity.y);
     }
 
     private void Jump() {
@@ -35,23 +50,32 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnMoveInput(InputAction.CallbackContext context) {
         Debug.Log("move input recieved");
-        moveInput = context.ReadValue<float>();
+        if (context.phase is InputActionPhase.Performed or InputActionPhase.Canceled) {
+            moveInput = context.ReadValue<float>();
+        }
     }
 
     public void OnJumpInput(InputAction.CallbackContext context) {
-        Debug.Log("jump input called");
         if(context.phase == InputActionPhase.Performed) {
             Debug.Log("jump input recieved");
-            if(jumpsAvailable > 0) {
-                jumpsAvailable--;
-                Jump();
-            }
+            lastJumpPressed = Time.time;
         }
     }
 
-    public void OnCollisionEnter2D() {
-        if (GetComponent<GroundCheck>().IsGrounded()) {
-                jumpsAvailable = maxJumps;
+    public void OnCollisionEnter2D(Collision2D collision) {
+        if (groundLayerTrue[collision.gameObject.layer]) {
+            touchingGround = true;
         }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision) {
+        if (groundLayerTrue[collision.gameObject.layer]) {
+            touchingGround = false;
+            isGrounded = false;
+        }
+    }
+
+    private void OnValidate() {
+        groundLayerTrue = new BitArray(new int[]{groundLayer.value}).Cast<bool>().ToArray();
     }
 }
